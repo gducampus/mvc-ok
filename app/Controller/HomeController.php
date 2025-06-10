f4c06da5975fb59f7922d17798ee2d44492da9ec
gducampus <gducampus@gmail.com>
2025-06-10 09:16:22 +0200

    initialisation

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+class HomeController
+{
+    public function index()
+    {
+        // This is the home page logic
+        return "Welcome to the Home Page!";
+    }
+
+
+
+
+
+}
